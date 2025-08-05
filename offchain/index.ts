a67097f03275d6830c5e08e25f4a0264a9cce436
a-osiecki <a.osiecki@mi.unc.edu.ar>
2025-08-05 13:08:35 -0300

    clean up p2p swaps

--- offchain/index.ts
+++ offchain/index.ts
@@ -1,8 +1,10 @@
 import { Client } from "./protocol.js";
 import { sha512 } from "@noble/hashes/sha2";
 import * as ed from "@noble/ed25519";
+import { U5C } from "@utxorpc/blaze-provider";
 import { VKeyWitness } from "tx3-sdk/trp";
 import "dotenv/config";
+import { Address, NetworkId } from "@blaze-cardano/core";
 
 ed.etc.sha512Sync = sha512;
 
@@ -14,6 +16,11 @@ const BOB = {
   privateKey: process.env.BOB_PRIVATE_KEY!,
   address: process.env.BOB_ADDRESS!,
 };
+
+const provider = new U5C({
+  url: "http://localhost:8164",
+  network: NetworkId.Testnet,
+});
 
 function sign(txHash: string, hexKey: string): VKeyWitness {
   const toSign = ed.etc.hexToBytes(txHash);
@@ -33,12 +40,26 @@ function sign(txHash: string, hexKey: string): VKeyWitness {
   };
 }
 
-async function lockFlow(): Promise<string> {
-  const resolved = await client.lockTx({
-    quantity: 5_000_000,
-    until: 200,
-    owner: BOB.address,
-    beneficiary: BOB.address,
+console.log(BOB.address);
+const bobPk = Address.fromBech32(BOB.address).getProps().paymentPart!.hash;
+
+async function placeOrderFlow(): Promise<string> {
+  console.log(
+    await provider.getUnspentOutputs(Address.fromBech32(BOB.address))
+  );
+
+  const resolved = await client.placeOrderTx({
+    maker: {
+      type: "String",
+      value: BOB.address,
+    },
+    offeredAmount: { type: "Int", value: 1_234_567n },
+    offeredName: { type: "String", value: "" },
+    offeredPolicy: { type: "String", value: "" },
+    recipientPaymentKey: { type: "String", value: bobPk },
+    requestedAmount: { type: "Int", value: 42_424_242n },
+    requestedName: { type: "String", value: "" },
+    requestedPolicy: { type: "String", value: "" },
   });
 
   console.log(resolved);
@@ -56,10 +77,29 @@ async function lockFlow(): Promise<string> {
   return `${resolved.hash}#0`;
 }
 
-async function unlockFlow(lockedUtxo: string) {
-  const resolved = await client.unlockTx({
-    beneficiary: BOB.address,
-    locked: lockedUtxo,
+async function swapFlow(lockedUtxo: string) {
+  const txid = Buffer.from(lockedUtxo.split("#")[0], "hex");
+  const index = parseInt(lockedUtxo.split("#")[1]);
+
+  const resolved = await client.swapTx({
+    orderRef: {
+      type: "UtxoRef",
+      value: {
+        txid,
+        index,
+      },
+    },
+    recipient: {
+      type: "String",
+      value: BOB.address,
+    },
+    requestedAmount: { type: "Int", value: 42_424_242n },
+    requestedName: { type: "String", value: "" },
+    requestedPolicy: { type: "String", value: "" },
+    taker: {
+      type: "String",
+      value: BOB.address,
+    },
   });
 
   console.log(resolved);
@@ -77,8 +117,8 @@ async function unlockFlow(lockedUtxo: string) {
   console.log(`utxo ${resolved.tx}#0 unlocked`);
 }
 
-const lockedUtxo = await lockFlow();
+const lockedUtxo = await placeOrderFlow();
 
-console.log(`utxo ${lockedUtxo} locked in vault`);
+console.log(`utxo ${lockedUtxo} locked in order`);
 
 // unlockFlow("abc#0");
